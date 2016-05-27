3cd400e83196a0178e3075c8903204c5c3281e8a
¨zzfancitizen@gmail.com¨ <zzfancitizen@gmail.com>
2016-05-27 13:28:12 +0000

    ¨led¨

---
+++ led.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+while True:
+	
+	GPIO.output(11, Ture)
+
+	time.sleep(1)
+	GPIO.output(11, False)
+	time.sleep(1)
+
+
